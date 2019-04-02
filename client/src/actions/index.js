65572a01b6f07ed54b111105163a7aec36bd4b1a
shashank99 <shashank.bittu@gmail.com>
2019-04-01 21:42:31 -0500

    commit from ogden 4/1

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,20 @@
-export const signIn=(userId)=>{
+import streams from '../apis/streams';
+import{SIGN_IN, SIGN_OUT} from './types'
 
+export const signIn=(userId)=>{
     return {
-        type:'SIGN_IN',
+        type:SIGN_IN,
         payload:userId
     };
-}
+};
 
 export const signOut=()=>{
-
     return {
-        type:'SIGN_OUT'
+        type:SIGN_OUT
     };
+};
+
+export const createStream=(formValues)=> async(dispatch)=>{
+    
+    streams.post('/streams',formValues);
 }
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions'
 
 class  StreamCreate extends React.Component{
 
@@ -26,8 +28,8 @@ renderError({error,touched}){
         ); 
     }
 
-    onSubmit(formValues){
-        console.log(formValues);
+    onSubmit=(formValues)=>{
+        this.props.createStream(formValues);
     }
    render(){
        console.log(this.props);
@@ -54,7 +56,9 @@ const validate=(formValues)=>{
     return errors;
 }
 
-export default reduxForm({
+const formWrapped= reduxForm({
     form:'streamCreate',
     validate:validate // form should know that there is a validate function
-})(StreamCreate)
+})(StreamCreate);
+
+export default connect(null,{createStream})(formWrapped);
